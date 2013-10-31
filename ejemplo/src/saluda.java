ae29122ec98c125adbfeff492bde729941997fc4
sebascampoamor@gmail.com <sebascampoamor@gmail.com@d0d167c8-608e-6647-9160-4dee127998c7>
2013-10-31 02:49:03 +0000

    cambio linea para probar commit

--- ejemplo/src/saluda.java
+++ ejemplo/src/saluda.java
@@ -9,7 +9,6 @@ public class saluda {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo");
-		System.out.println("Hola mundo");
 		double tuna;
 		tuna = 3.14;
 		System.out.println(tuna);
