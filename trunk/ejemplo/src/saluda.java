5e19776b353b57b7c670ac55016ef09a52c47f51
sebascampoamor@gmail.com <sebascampoamor@gmail.com@d0d167c8-608e-6647-9160-4dee127998c7>
2013-10-31 02:45:46 +0000

    subo el ejemplo para probar que puedo commitear

---
+++ trunk/ejemplo/src/saluda.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+public class saluda {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
+		System.out.println("Hola mundo");
+		double tuna;
+		tuna = 3.14;
+		System.out.println(tuna);
+
+		Scanner apple = new Scanner(System.in);
+	    System.out.println(apple.nextLine());
+	    
+	}
+
+}
