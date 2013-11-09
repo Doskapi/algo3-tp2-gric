56ae2212afb25ebcf3fd5519fbb1debbf0c84314
sebascampoamor@gmail.com <sebascampoamor@gmail.com@d0d167c8-608e-6647-9160-4dee127998c7>
2013-11-09 19:21:03 +0000

    [REUNION EN GRuPO SABASO 9/11]
    agrego clase  y  test de obstaculo

---
+++ GPSChallenge/src/ar/fi/uba/GPSChallengeTests/ObstaculoTest.java
@@ -0,0 +1,15 @@
+package ar.fi.uba.GPSChallengeTests;
+
+import org.junit.Test;
+
+import ar.fi.uba.GPSChallenge.Modelo.Obstaculo;
+import junit.framework.TestCase;
+
+public class ObstaculoTest extends TestCase {
+
+	public void testCreoObstaculo() {
+		Obstaculo obstaculo = new Obstaculo();
+		assertNotNull(obstaculo);
+	}
+
+}
