76350c59969412a1fd6d19db6a56e68df05d5fb5
ignacio.goicoa@gmail.com <ignacio.goicoa@gmail.com@d0d167c8-608e-6647-9160-4dee127998c7>
2013-11-09 20:01:31 +0000

    [REUNION EN GRUPO SABADO 09/11]
    Agrego clase Posicion y sus tests

---
+++ GPSChallenge/src/ar/fi/uba/GPSChallengeTests/PosicionTests.java
@@ -0,0 +1,22 @@
+package ar.fi.uba.GPSChallengeTests;
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+import ar.fi.uba.GPSChallenge.Modelo.Posicion;
+
+public class PosicionTests extends TestCase {
+
+	@Test
+	public void testCreoUnaPosicionVacia(){
+		Posicion posicion = new Posicion();
+		assertNotNull (posicion);
+	}
+	
+	@Test
+	public void testCreoUnaPosicionValida(){
+		Posicion posicion = new Posicion(1,2);
+		assertNotNull (posicion);
+	}
+	
+}
