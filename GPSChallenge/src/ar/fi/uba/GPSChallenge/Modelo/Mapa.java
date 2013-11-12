5afcbfa7fed6d550c1c7837b5a8e7cd5f08fe6b9
ignacio.goicoa@gmail.com <ignacio.goicoa@gmail.com@d0d167c8-608e-6647-9160-4dee127998c7>
2013-11-12 00:19:05 +0000

    Agrego la creacion del mapa solo con la esquina largada y la esquina meta. La esquina largada se setea en forma automatica (siempre a mitad de mapa) y la esquina meta se setea en forma automatica haciendo un random para que caiga en cualquier fila dentro de las dimensiones del mapa. Estan testeados todos los casos.

--- GPSChallenge/src/ar/fi/uba/GPSChallenge/Modelo/Mapa.java
+++ GPSChallenge/src/ar/fi/uba/GPSChallenge/Modelo/Mapa.java
@@ -2,8 +2,67 @@ package ar.fi.uba.GPSChallenge.Modelo;
 
 public class Mapa {
 	
+	private int filas;
+	private int columnas;
 	Esquina largada;
 	Esquina meta;
 	Vehiculo vehiculo;
 
+	public Mapa(int filas, int columnas){
+		this.filas = filas;
+		this.columnas = columnas;
+		setLargadaAutomaticamente();
+		setMetaEnFormaAleatoria();
+	}
+
+	private void setLargadaAutomaticamente() {
+		int columnaLargada = 1;
+		int filaLargada = this.filas / 2;
+		Esquina esquinaLargada = new Esquina(new Posicion(filaLargada, columnaLargada));
+		this.largada = esquinaLargada;
+	}
+	
+	private void setMetaEnFormaAleatoria() {
+		int columnaMeta = this.columnas;
+		int filaMeta = (1 + (int)(Math.random() * ((this.filas - 1) + 1)));
+		Esquina esquinaMeta = new Esquina(new Posicion(filaMeta, columnaMeta));
+		this.meta = esquinaMeta;
+	}
+
+	public int dimensiones(){
+		return((this.filas) * (this.columnas));
+	}
+	
+	public int cantidadFilas(){
+		return this.filas;
+	}
+	
+	public int cantidadColumnas(){
+		return this.columnas;
+	}
+
+	public void setEsquinaLargada(Esquina esquina){
+		this.largada = esquina;
+	}
+	
+	public Esquina getEsquinaLargada(){
+		return(this.largada);
+	}
+
+	public void setEsquinaMeta(Esquina esquina){
+		this.meta = esquina;
+	}
+	
+	public Esquina getEsquinaMeta(){
+		return(this.meta);
+	}
+
+	public void agregarVehiculo(Vehiculo vehiculo){
+		this.vehiculo = vehiculo;
+	}
+	
+	public Vehiculo devolverVehiculo(){
+		return(this.vehiculo);
+	}
+	
 }
--- GPSChallenge/src/ar/fi/uba/GPSChallengeTests/MapaTests.java
+++ GPSChallenge/src/ar/fi/uba/GPSChallengeTests/MapaTests.java
@@ -8,10 +8,72 @@ public class MapaTests extends TestCase {
 	
 	@Test
 	public void testCreoUnMapaVacio(){
-		Mapa mapa = new Mapa();
+		Mapa mapa = new Mapa(10,10);
 		assertNotNull (mapa);
 	}
 	
-
+	@Test
+	public void testCreoUnMapaDe10x12(){
+		Mapa mapa = new Mapa(10,12);
+		assertEquals(mapa.dimensiones(),120);
+	}
+	
+	@Test
+	public void testCreoUnMapaDe10x12YLeColocoUnaEsquinaDeLargada(){
+		Mapa mapa = new Mapa(10,12);
+		assertEquals(mapa.dimensiones(),120);
+		Esquina esquina = new Esquina(new Posicion(6,1));
+		mapa.setEsquinaLargada(esquina);
+		assertEquals(mapa.getEsquinaLargada(), esquina);
+	}
+	
+	@Test
+	public void testCreoUnMapaDe10x12YLeColocoUnaEsquinaDeMeta(){
+		Mapa mapa = new Mapa(10,12);
+		assertEquals(mapa.dimensiones(),120);
+		Esquina esquina = new Esquina(new Posicion(9,12));
+		mapa.setEsquinaMeta(esquina);
+		assertEquals(mapa.getEsquinaMeta(), esquina);
+	}
+	
+	@Test
+	public void testCreoUnMapaDe10x12YLeColocoUnVehiculo(){
+		Mapa mapa = new Mapa(10,12);
+		assertEquals(mapa.dimensiones(),120);
+		Vehiculo vehiculo = new Vehiculo(new Auto());
+		mapa.agregarVehiculo(vehiculo);
+		assertEquals(mapa.devolverVehiculo(), vehiculo);
+	}
+	
+	@Test
+	public void testCreoUnMapaDe10x12YLaLargadaSeColocaAutomaticamenteEnMitadDeMapa(){
+		Mapa mapa = new Mapa(10,12);
+		Esquina esquina = new Esquina(new Posicion(5,1));
+		assertEquals(mapa.dimensiones(),120);
+		assertEquals(mapa.getEsquinaLargada(), esquina);
+	}
+	
+	@Test
+	public void testCreoUnMapaDe11x12YLaLargadaSeColocaAutomaticamenteEnMitadDeMapa(){
+		Mapa mapa = new Mapa(11,12);
+		Esquina esquina = new Esquina(new Posicion(5,1));
+		assertEquals(mapa.dimensiones(),132);
+		assertEquals(mapa.getEsquinaLargada(), esquina);
+	}
+	
+	@Test
+	public void testCreoUnMapaDe15x10YLaLargadaSeColocaAutomaticamenteEnMitadDeMapa(){
+		Mapa mapa = new Mapa(15,10);
+		Esquina esquina = new Esquina(new Posicion(7,1));
+		assertEquals(mapa.dimensiones(),150);
+		assertEquals(mapa.getEsquinaLargada(), esquina);
+	}
+	
+	@Test
+	public void testCreoUnMapaDe10x12YLaMetaSeColocaEnFormaAleatoria(){
+		Mapa mapa = new Mapa(10,12);
+		assertEquals(mapa.dimensiones(),120);
+		assertTrue(mapa.getEsquinaMeta().getPosicion().getColumna() == 12);
+	}
 }
 
