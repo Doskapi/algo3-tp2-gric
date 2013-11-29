3da73c39d84bde6db3fede504217492a0e5a0178
ignacio.goicoa@gmail.com <ignacio.goicoa@gmail.com@d0d167c8-608e-6647-9160-4dee127998c7>
2013-11-29 13:19:29 +0000

    [REUNION EN GRUPO 29/11] Se agrega un PanelConFondo en la Vista (JPanel para guardar imagenes)

--- GPSChallenge/src/ar/fi/uba/GPSChallenge/Vista/VistaPrincipal.java
+++ GPSChallenge/src/ar/fi/uba/GPSChallenge/Vista/VistaPrincipal.java
@@ -3,6 +3,17 @@ package ar.fi.uba.GPSChallenge.Vista;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import javax.swing.JPanel;
+import java.awt.GridBagConstraints;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import java.awt.Color;
+import java.awt.Font;
 
 public class VistaPrincipal {
 
@@ -36,8 +47,20 @@ public class VistaPrincipal {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 735, 468);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panelConFondo = new PanelConFondo("Estadio40.jpg");
+		
+		panelConFondo.setBounds(111, 72, 505, 341);
+		frame.getContentPane().add(panelConFondo);
+		
+		JLabel lblRojoPutoSos = new JLabel("ROJO PUTO SOS DE LA B");
+		lblRojoPutoSos.setFont(new Font("Dialog", Font.BOLD, 18));
+		lblRojoPutoSos.setForeground(new Color(0, 0, 255));
+		lblRojoPutoSos.setToolTipText("");
+		lblRojoPutoSos.setBounds(234, 26, 338, 22);
+		frame.getContentPane().add(lblRojoPutoSos);
 	}
-
 }
