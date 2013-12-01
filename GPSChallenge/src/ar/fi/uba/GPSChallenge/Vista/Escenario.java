2acb879a16022e98f02d2f315eaa9ef98014d772
ignacio.goicoa@gmail.com <ignacio.goicoa@gmail.com@d0d167c8-608e-6647-9160-4dee127998c7>
2013-12-01 22:38:22 +0000

    [REUNION EN GRUPO 01/12] Se mejora el tamaño del escenario.

--- GPSChallenge/src/ar/fi/uba/GPSChallenge/Vista/Escenario.java
+++ GPSChallenge/src/ar/fi/uba/GPSChallenge/Vista/Escenario.java
@@ -12,9 +12,10 @@ import java.awt.Font;
 
 public class Escenario extends JPanel {
 	
-	PanelConFondo celdas[][] = new PanelConFondo[30][30];
-	Image imagenEsquinas = new ImageIcon(getClass().getResource("Esquinas.jpg")).getImage();
+	PanelConFondo celdas[][] = new PanelConFondo[28][28];
+	Image imagenCiudad = new ImageIcon(getClass().getResource("MapaGrande.png")).getImage();
 	Image imagenAuto = new ImageIcon(getClass().getResource("Auto.png")).getImage();
+	Image imagenCuatroPorCuatro = new ImageIcon(getClass().getResource("CuatroPorCuatroOriginal.png")).getImage();
 	Image imagenTransparente = new ImageIcon(getClass().getResource("ImagenTransparente.png")).getImage();
 	PanelConFondo grilla;
 	JPanel informacion;
@@ -27,16 +28,17 @@ public class Escenario extends JPanel {
 	public Escenario(VistaPrincipal vistaPrincipal, Controlador controlador) {
 		this.vistaPrincipal = vistaPrincipal;
 		this.controlador = controlador;
-		setLayout(new BorderLayout(0, 0));
+		setLayout(null);
 		
 		grilla = new PanelConFondo();
-		grilla.setImagen(imagenEsquinas);
+		grilla.setBounds(10, 10, 700, 700);
+		grilla.setImagen(imagenCiudad);
 		
 		informacion = new JPanel();
-		add(informacion, BorderLayout.EAST);
+		add(informacion);
 		
 		titulo = new JPanel();
-		add(titulo, BorderLayout.NORTH);
+		add(titulo);
 		
 		lblGpschallenge = new JLabel("GPSChallenge ");
 		lblGpschallenge.setFont(new Font("Dialog", Font.BOLD, 12));
@@ -44,22 +46,24 @@ public class Escenario extends JPanel {
 		
 		JLabel lblHola = new JLabel("HOLA");
 		informacion.add(lblHola);
-		grilla.setLayout(new GridLayout(30, 30, 0, 0));
-		add(grilla, BorderLayout.CENTER);
 		
-        for (int i = 0; i < 30; i++) {
-        	for (int j = 0; j < 30; j++) {
+		grilla.setLayout(new GridLayout(28, 28, 0, 0));
+		add(grilla);
+		
+        for (int i = 0; i < 28; i++) {
+        	for (int j = 0; j < 28; j++) {
         		celdas[i][j] = new PanelConFondo();
                 grilla.add(celdas[i][j]);
             }
         }         
      
-        for (int i = 0; i < 30; i++) {
-        	for (int j = 0 ; j < 30; j++) {
+        for (int i = 0; i < 28; i++) {
+        	for (int j = 0 ; j < 28; j++) {
         		celdas[i][j].setImagen(imagenTransparente);
             }
         }
-        celdas[4][5].setImagen(imagenAuto);
+        celdas[0][3].setImagen(imagenAuto);
+     
 	}
 }
 	
