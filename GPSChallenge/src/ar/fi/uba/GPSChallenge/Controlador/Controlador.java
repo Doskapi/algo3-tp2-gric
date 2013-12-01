95b22b0e7be35fc9959877e765cc37c02a8c47c1
riegoleonardo@gmail.com <riegoleonardo@gmail.com@d0d167c8-608e-6647-9160-4dee127998c7>
2013-12-01 14:37:16 +0000

    [REUNIONE EN GRUPO JUEVES 1/12]
    Cambiado controlador para que persista un jugador cuando se guarda.

--- GPSChallenge/src/ar/fi/uba/GPSChallenge/Controlador/Controlador.java
+++ GPSChallenge/src/ar/fi/uba/GPSChallenge/Controlador/Controlador.java
@@ -1,9 +1,11 @@
 package ar.fi.uba.GPSChallenge.Controlador;
 
-import java.awt.event.ActionListener;
 import java.util.List;
 
-import ar.fi.uba.GPSChallenge.Modelo.*;
+import ar.fi.uba.GPSChallenge.Modelo.Juego;
+import ar.fi.uba.GPSChallenge.Modelo.Jugador;
+import ar.fi.uba.GPSChallenge.Persistencia.PersistidorDeJuego;
+import ar.fi.uba.GPSChallenge.Persistencia.PersistidorJugador;
 
 public class Controlador {
 	
@@ -16,6 +18,20 @@ public class Controlador {
 	public void crearNuevoJugador(String usuarioNuevo) {
 		Jugador jugador = new Jugador(usuarioNuevo);
 		this.referenciaAlJuego.agregarJugador(jugador);
+		this.persistirJuego();
+		this.persistirJugador(jugador);
+	}
+
+	private void persistirJugador(Jugador jugador) {
+		PersistidorJugador persistidorJugador = new PersistidorJugador();
+		persistidorJugador.persistirJugador(jugador);
+		
+	}
+
+	private void persistirJuego() {
+		PersistidorDeJuego persistidorJuego = new PersistidorDeJuego();
+		persistidorJuego.persistirJuego(this.referenciaAlJuego);
+		
 	}
 
 	public List<Jugador> pedirJugadoresExistentes() {
