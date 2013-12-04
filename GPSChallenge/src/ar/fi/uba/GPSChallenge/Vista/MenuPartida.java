ee0e6572e9ad68ee7ba30a5148e37d01ad561f80
ignacio.goicoa@gmail.com <ignacio.goicoa@gmail.com@d0d167c8-608e-6647-9160-4dee127998c7>
2013-12-04 22:01:16 +0000

    [REUNION EN GRUPO 04/12] Refactorizacion

--- GPSChallenge/src/ar/fi/uba/GPSChallenge/Vista/MenuPartida.java
+++ GPSChallenge/src/ar/fi/uba/GPSChallenge/Vista/MenuPartida.java
@@ -44,7 +44,7 @@ public class MenuPartida extends JPanel {
 			}
 		});
 		
-		JButton btnContinuarConSuPartida = new JButton("Continuar Partida Guardada");
+		JButton btnContinuarConSuPartida = new JButton("Continuar Partida");
 		btnContinuarConSuPartida.setBounds(500, 300, 200, 70);
 		add(btnContinuarConSuPartida);
 		btnContinuarConSuPartida.addActionListener(new ActionListener() {
